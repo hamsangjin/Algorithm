f23772c1dc5bd10088f47bdf7e462eadee636273
HamSangJin <103736614+hamsangjin@users.noreply.github.com>
2023-06-03 19:31:19 +0900

    [Bronze V] Title: 대소문자 바꾸기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
+++ 백준/Bronze/2744. 대소문자 바꾸기/대소문자 바꾸기.py
@@ -1,12 +1,9 @@
 input = input()
-output_list = []
 output = ''
 for i in range(len(input)):
     if input[i].islower() == True:
-        output_list.append(input[i].upper())
+        output = output + input[i].upper()
     else:
-        output_list.append(input[i].lower())
+        output = output + input[i].lower()
 
-for i in output_list:
-    output = output + i
 print(output)
