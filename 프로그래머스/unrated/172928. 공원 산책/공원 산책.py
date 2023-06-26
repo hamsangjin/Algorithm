d8e012b2aba17b6fafff657854d2a81af77718b8
SangJin Ham <rhkstlash@naver.com>
2023-06-26 21:55:10 +0900

    [unrated] Title: 공원 산책, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/unrated/172928. 공원 산책/공원 산책.py
@@ -17,15 +17,21 @@ def solution(park, routes):
         # 방향과 거리 분할 입력
         direction, distance = cmd.split()
         
-        # 명령 실행 전 좌표 저장(이동 불가 시 되돌리기 위함)
+        # 명령 실행 전 nr, nc에 좌표 복사(r, c로 진행시 이동 불가 시 되돌리지 못하기 때문)
         nr, nc = r, c
+        
+        # 명령 성공 여부
         flag = True
         
         for i in range(int(distance)):
+            # 거리가 n이라면 1 ~ n까지 진행
             nr, nc = nr + dr[dic[direction]], nc + dc[dic[direction]]
+            
+            # 1 ~ n 중에서 하나라도 공원을 나가거나 장애물을 지나게 된다면 실패라고 설정하고 for문 벗어남
             if nr < 0 or nr >= n or nc < 0 or nc >= m or park[nr][nc] == 'X':
                 flag = False
                 break
+        # 성공 = 움직인 좌표 / 실패 = 기존 좌표
         if flag:
             r, c = nr, nc
 
