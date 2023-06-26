b2607b41b68959c1e02e1abad288883d17b2aa7f
SangJin Ham <rhkstlash@naver.com>
2023-06-26 21:37:48 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,22 +1,22 @@
 def solution(n):
-    dr = [0, 1, 0, -1]  # 오른쪽, 아래, 왼쪽, 위
-    dc = [1, 0, -1, 0]
-    answer = [[0] * n for _ in range(n)]
-    num = 1  # 채워질 숫자
-    r, c = 0, 0  # 현재 위치
-    d = 0  # 현재 방향 (오른쪽)
+    answer = [[0]*n for _ in range(n)] 
 
-    while num <= n * n:
-        answer[r][c] = num
-        num += 1
+    dr = [-1, 0, 1, 0]
+    dc = [0, 1, 0, -1]
 
+    r, c = 0, 0
+    d, cnt = 1, 1
+
+    while(n*n > cnt):
         nr = r + dr[d]
         nc = c + dc[d]
-        if nr < 0 or nr >= n or nc < 0 or nc >= n or answer[nr][nc] != 0:
+        if nr < 0 or nr >= n or nc < 0 or nc >=n or answer[nr][nc] != 0:
             d = (d + 1) % 4
-            nr = r + dr[d]
-            nc = c + dc[d]
-
-        r, c = nr, nc
-
+            continue 
+        answer[r][c] = cnt
+        cnt += 1
+        r = nr
+        c = nc
+    # if문으로 인해 마지막 원소 안 되므로 while문 끝나고 적용
+    answer[r][c] = cnt
     return answer
