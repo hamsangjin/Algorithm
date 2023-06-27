d6beee1d847de0301f9cd75db7ea8f35a1595a80
SangJin Ham <rhkstlash@naver.com>
2023-06-27 16:15:19 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+def solution(array):
+    counter = Counter(array)  # 각 원소의 등장 횟수를 카운트하는 Counter 객체 생성
+    max_count = max(counter.values())  # 등장 횟수의 최댓값을 찾음
+
+    # 최빈값이 여러 개인지 확인
+    mode_values = [num for num, count in counter.items() if count == max_count]
+    if len(mode_values) > 1:
+        return -1  # 최빈값이 여러 개면 -1 반환
+
+    return mode_values[0]  # 최빈값 반환
