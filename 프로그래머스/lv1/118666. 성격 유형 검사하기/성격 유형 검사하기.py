6f11a9c1daa482925df76d9a07fb17a4e7f46483
SangJin Ham <rhkstlash@naver.com>
2023-06-28 10:47:05 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.65 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -1,36 +1,19 @@
 from collections import defaultdict
 
-    # A : 어피치
-    # N : 네오
-    # C : 콘
-    # P : 프로도
-
 def solution(survey, choices):
     answer = ''
     points = {"1": 3, "2": 2, "3": 1, "4": 0, "5": 1, "6": 2, "7": 3}
-    categories = [['R', 'T'], ['C', 'F'], ['J', 'M'], ['A', 'N']]
+    categories = ['RT', 'CF', 'JM', 'AN']
     sH = defaultdict(int)
 
     for i in range(len(survey)):
-        # 매우 미동의 ~ 약간 비동의
         if choices[i] <= 3:
-            # print('choices <= 3 : ', points[str(choices[i])])
             sH[survey[i][0]] += points[str(choices[i])]
 
-        # 약간동의 ~ 매우 동의
         elif choices[i] >= 5:
-            # print('choices >= 3 : ', points[str(choices)])
             sH[survey[i][1]] += points[str(choices[i])]
 
-        # 모르겠음 그냥 넘어감
-        else:
-            continue
-
-    # print(sH)
-
     for category in categories:
-        # print('sH[category[0]] = ', sH[category[0]])
-        # print('sH[category[1]] = ', sH[category[1]])
         if sH[category[0]] > sH[category[1]]:
             answer += category[0]
         elif sH[category[0]] < sH[category[1]]:
