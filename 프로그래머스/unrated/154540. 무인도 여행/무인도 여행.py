8a750dc4a8e29a77341673446263c408a089d0f7
SangJin Ham <rhkstlash@naver.com>
2023-06-30 00:05:05 +0900

    [unrated] Title: 무인도 여행, Time: 0.18 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.py
@@ -1,37 +1,35 @@
 import sys
-# 런타임오류가 발생하면 아래 줄을 작성
-# 제한을 늘려야 함
 sys.setrecursionlimit(int(1e6))
 
-
 dr = [-1, 0, 1, 0]
-dc = [0, 1, 0 , -1]
+dc = [0, 1, 0, -1]
 cnt = 0
-def DFS(r, c, n, m, board):
+
+def DFS(r, c, n, m, maps):
     global cnt
-    cnt += int(board[r][c])
-    board[r][c] = 'X'
+    cnt += int(maps[r][c])
+    maps[r][c] = 'X'
+    
     for i in range(4):
         nr = r + dr[i]
         nc = c + dc[i]
-        if nr >= 0 and nr < n and nc >= 0 and nc < m and board[nr][nc] != 'X':
-            DFS(nr, nc, n, m, board)
+        if nr >= 0 and nr < n and nc >= 0 and nc < m and maps[nr][nc] != 'X':
+            DFS(nr, nc, n, m, maps)
 
 def solution(maps):
     global cnt
-    board = [list(row) for row in maps]
     n = len(maps)
     m = len(maps[0])
     answer = []
+    maps = [list(row) for row in maps]
+    
     for i in range(n):
         for j in range(m):
-            if board[i][j] != 'X':
+            if maps[i][j] != 'X':
                 cnt = 0
-                DFS(i, j, n, m, board)
+                DFS(i, j, n, m, maps)
                 answer.append(cnt)
     if answer:
         return sorted(answer)
     else:
         return [-1]
-
-print(solution(["X591X","X1X5X","X231X", "1XXX1"]))
