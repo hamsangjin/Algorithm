131cc6fe8970539bfa278bdddb5452bd072b86f1
SangJin Ham <rhkstlash@naver.com>
2023-06-29 21:21:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,33 +1,33 @@
 from collections import deque
+
 def solution(maps):
     n = len(maps)
     m = len(maps[0])
-
-    dist = [[0]*m for _ in range(n)]
     
     dr = [-1, 0, 1, 0]
     dc = [0, 1, 0, -1]
     
+    dist = [[0]*m for _ in range(n)]
+    
     Q = deque()
     Q.append([0, 0])
     maps[0][0] = 0
-
-    # 시작 좌표도 포함하므로 L = 1
     L = 1
     
     while Q:
         length = len(Q)
         for _ in range(length):
-            r, c = Q.popleft()
+            v = Q.popleft()
             for i in range(4):
-                nr = r + dr[i]
-                nc = c + dc[i]
-                if  0 <= nr and nr < n and 0 <= nc and nc < m and maps[nr][nc] == 1:
-                    Q.append([nr, nc])
+                nr = v[0] + dr[i]
+                nc = v[1] + dc[i]
+                if nr >= 0 and nr < n and nc >= 0 and nc < m and maps[nr][nc] == 1:
+                    dist[nr][nc] = L+1
                     maps[nr][nc] = 0
-                    dist[nr][nc] = L + 1   
-        L += 1    
-    if dist[n-1][m-1] == 0:
-        return -1
-    else:
+                    Q.append([nr, nc])
+        L += 1
+    
+    if dist[n-1][m-1]:
         return dist[n-1][m-1]
+    else:
+        return -1
