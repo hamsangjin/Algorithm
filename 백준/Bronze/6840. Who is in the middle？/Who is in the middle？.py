982b7c697d925112f154cc28b888ab5d61e1d76b
SangJin Ham <rhkstlash@naver.com>
2023-09-25 14:23:04 +0900

    [Bronze V] Title: Who is in the middle?, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.py
@@ -0,0 +1,7 @@
+import sys
+
+ans = []
+ans.append(int(sys.stdin.readline().strip()))
+ans.append(int(sys.stdin.readline().strip()))
+ans.append(int(sys.stdin.readline().strip()))
+print(sorted(ans)[1])
