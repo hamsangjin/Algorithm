ca055220696b8078000cccedcb0264cb359446c2
SangJin Ham <rhkstlash@naver.com>
2023-09-26 14:44:22 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,5 @@
+import sys
+
+schools = {'NLCS': 'North London Collegiate School', 'BHA': 'Branksome Hall Asia', 'KIS': 'Korea International School', 'SJA': 'St. Johnsbury Academy'}
+
+print(schools[sys.stdin.readline().strip()])
