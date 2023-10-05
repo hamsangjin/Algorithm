3f45e9906fa1300ee2f931b0fd64dbb8d1c4039d
SangJin Ham <rhkstlash@naver.com>
2023-10-05 13:20:26 +0900

    [Bronze V] Title: Lucky 7, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/30224. Lucky 7/Lucky 7.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+
+if '7' not in str(N) and N % 7 != 0:
+  print(0)
+elif '7' not in str(N) and N % 7 == 0:
+  print(1)
+elif '7' in str(N) and N % 7 != 0:
+  print(2)
+elif '7' in str(N) and N % 7 == 0:
+  print(3)
