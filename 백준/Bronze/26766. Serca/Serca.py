a052065db695c05c7c0a90091406117595f6f8fe
SangJin Ham <rhkstlash@naver.com>
2023-11-04 19:14:27 +0900

    [Bronze V] Title: Serca, Time: 448 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,14 @@
+import sys
+
+A = int(sys.stdin.readline().strip())
+
+for _ in range(A):
+  print(' @@@   @@@ ')
+  print('@   @ @   @')
+  print('@    @    @')
+  print('@         @')
+  print(' @       @ ')
+  print('  @     @  ')
+  print('   @   @   ')
+  print('    @ @    ')
+  print('     @     ')
