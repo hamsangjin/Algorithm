0c213992ade5ec0bf6532ef373feffd223c0efdb
SangJin Ham <rhkstlash@naver.com>
2023-11-23 13:48:14 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,16 +1,34 @@
 from collections import defaultdict
 
 def solution(genres, plays):
-    dic1, dic2 = defaultdict(list), defaultdict(int)
-    answer = []
+    dict = defaultdict(list)
+    best = []
+    n = len(genres)
 
-    for i, (g, p) in enumerate(zip(genres, plays)):
+    for i in range(n):
+        if len(dict[genres[i]]) == 0:
+            dict[genres[i]] = []
+            dict[genres[i]].append([plays[i], i])
+        else:
+            dict[genres[i]].append([plays[i], i])
+    
+    for genre in dict:
+        dict[genre].sort(key=lambda x: (-x[0], x[1]))
 
-        dic1[g].append((i, p))
-        dic2[g] += p
+    high = []
+    for genre in dict:
+        temp = 0
+        for i in range(len(dict[genre])):
+            temp += dict[genre][i][0]
+        high.append([genre, temp])
+    high.sort(key = lambda x : -x[1])
 
-    for (g, _) in sorted(dic2.items(), key = lambda x : -x[1]):
-        for (i, p) in sorted(dic1[g], key=lambda x:-x[1])[:2]:
-            answer.append(i)
+    for genre, _ in high:
+        if len(dict[genre]) == 1:
+            best.append(dict[genre][0][1])
+        else:
+            best.append(dict[genre][0][1])
+            best.append(dict[genre][1][1])
+    return best
 
-    return answer
+print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 800, 800, 2500]))
