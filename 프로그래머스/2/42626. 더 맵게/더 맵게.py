fa30b381bc730116d8b20b94a579324645f82577
SangJin Ham <rhkstlash@naver.com>
2023-12-05 18:14:10 +0900

    [level 2] Title: 더 맵게, Time: 1654.42 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,3 @@
-import heapq
-from collections import deque
-
 def solution(scoville, K):
     answer = 0
-    
-    heap = []
-    for x in scoville:
-        heapq.heappush(heap, x)
-    
-    while heap[0] < K:
-        try:
-            heapq.heappush(heap, heapq.heappop(heap) + heapq.heappop(heap) * 2)
-        except IndexError:
-            return -1
-        
-        answer += 1
     return answer
-print(solution([1, 2, 3, 9, 10, 12], 200))
