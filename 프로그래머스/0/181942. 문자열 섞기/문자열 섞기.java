c9552e2b4fb1b2a514b9cb314484633cb9bbac57
SangJin Ham <rhkstlash@naver.com>
2024-01-27 13:30:43 +0900

    [level 0] Title: 문자열 섞기, Time: 1.28 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,14 +1,12 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        
-        int len = str1.length();
-        
-        for(int i = 0; i < len; i++){
-            answer += str1.charAt(i);
-            answer += str2.charAt(i);
+
+        for(int i = 0; i < str1.length(); i++) {
+            answer += String.valueOf(str1.charAt(i));
+            answer += String.valueOf(str2.charAt(i));
         }
-        
+
         return answer;
     }
 }
