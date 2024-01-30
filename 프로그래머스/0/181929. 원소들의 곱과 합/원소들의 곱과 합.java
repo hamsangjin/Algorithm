2d04a55ac495bbf3e30e4842b744c994f994152b
SangJin Ham <rhkstlash@naver.com>
2024-01-30 14:25:17 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int sum1 = 1;
+        int sum2 = 0;
+        
+        for(int num : num_list){
+            sum1 *= num;
+            sum2 += num;
+        }
+        
+        return sum1 < sum2*sum2 ? 1 : 0;
+    }
+}
