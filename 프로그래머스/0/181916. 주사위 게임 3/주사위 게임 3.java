d0d97c8494e78b5d0ecf3091a94f4e7ede08f6df
SangJin Ham <rhkstlash@naver.com>
2024-01-31 23:18:03 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.55 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,51 +1,34 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int a, int b, int c, int d) {
-        // 네 주사위 모두 같은 숫자
-        if (a == b && b == c && c == d){
-                return 1111 * a;
-        }
-
+        
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+        
+        int answer = 0;
+        
+        // 네 주사위 모두 같은 숫자  
+        if (dice[0] == dice[3]){           
+            answer = 1111 * dice[0];
         // 세 주사위 같은 숫자, 하나 다름
-        if (((a == b && b == c) && c != d)){
-                return (10 * a + d) * (10 * a + d);
-        } else if (((b == c && c == d) && d != a)){
-                return (10 * b + a) * (10 * b + a);
-        } else if (((a == c && c == d) && d != b)){
-                return (10 * a + b) * (10 * a + b);
-        } else if (((a == b && b == d) && d != c)){
-                return (10 * a + c) * (10 * a + c);
-        }
-
+        } else if ((dice[0] == dice[2]) || (dice[1] == dice[3])){                   
+            answer = (int) Math.pow((10 * dice[1] + (dice[0] + dice[3] - dice[1])), 2);
         // 두 주사위끼리 서로 다른 쌍
-        if ((a == b && c == d) && a != d){
-                return (a + d) * Math.abs(a - d);
-        } else if ((a == c && b == d) && a != d){
-                return (a + d) * Math.abs(a - d);
-        } else if ((a == d && b == c) && a != c){
-                return (a + c) * Math.abs(a - c);
-        }
-
+        } else if (dice[0] == dice[1] && dice[2] == dice[3]){                       
+                answer = (dice[0] + dice[3]) * (dice[3] - dice[0]);
         // 두 주사위는 같으면서, 나머지 두 주사위끼리는 같지 않을 때
-        if (a == b && b != c && c != d){
-                return c * d;
-        } else if (a == c && b != c && b != d){
-                return b * d;
-        } else if (a == d && b != d && b != c){
-                return b * c;
-        } else if (b == c && a != b && a != d){
-                return a * d;
-        } else if (b == d && a != b && a != c){
-                return a * c;
-        } else if (c == d && a != c && a != b){
-                return a * b;
-        }
-
-
+        } else if (dice[0] == dice[1]){
+                answer = dice[2] * dice[3];
+        } else if (dice[1] == dice[2]){
+                answer = dice[0] * dice[3];
+        } else if (dice[2] == dice[3]){
+                answer = dice[0] * dice[1];
         // 네 주사위가 다른 경우
-        if (a != b && a != c && a != d && b != c && b != d && c != d){
-                return Math.min(Math.min(a, b), Math.min(c, d));
+        } else if (dice[0] != dice[1] && dice[1] != dice[2] && dice[2] != dice[3]){ 
+                answer = dice[0];
         }
         
-        return 0;
+        return answer;
     }
 }
