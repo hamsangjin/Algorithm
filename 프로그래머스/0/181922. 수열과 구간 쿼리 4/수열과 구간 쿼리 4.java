0fe5fe6f35788de96d8e380c696d4a346758f52c
SangJin Ham <rhkstlash@naver.com>
2024-02-01 16:30:21 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 3.00 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -1,8 +1,7 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         
-        for(int i = 0; i < queries.length; i++){
-            int[] query = queries[i];
+        for(int[] query : queries){
             int s = query[0], e = query[1], k = query[2];
             for(int j = s; j <= e; j++){
                 arr[j] += (j % k == 0) ? 1 : 0;
