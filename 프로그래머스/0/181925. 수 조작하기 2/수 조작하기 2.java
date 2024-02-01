8f92a133e02a61d95b555c8dbed50fb293f2a6f2
SangJin Ham <rhkstlash@naver.com>
2024-02-01 14:55:07 +0900

    [level 0] Title: 수 조작하기 2, Time: 658.91 ms, Memory: 379 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -4,14 +4,12 @@ class Solution {
         int temp = 0;
 
         for (int i = 1; i < numLog.length; i++){
-            if (numLog[i] - numLog[i-1] == 1){
-                answer += "w";
-            } else if (numLog[i] - numLog[i-1] == -1){
-                answer += "s";
-            } else if (numLog[i] - numLog[i-1] == 10){
-                answer += "d";
-            } else if (numLog[i] - numLog[i-1] == -10){
-                answer += "a";
+            int x = numLog[i] - numLog[i-1];
+            switch(x){
+                case 1: answer += "w"; break;
+                case -1: answer += "s"; break;
+                case 10: answer += "d"; break;
+                case -10: answer += "a"; break;
             }
         }
         
