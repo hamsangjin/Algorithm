fd46859c16ea97b9eaa38bbb9b1e30f2bcd7a658
SangJin Ham <rhkstlash@naver.com>
2024-02-03 16:54:28 +0900

    [level 0] Title: 그림 확대, Time: 1.65 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] picture, int k) {
+        int n = picture.length;
+        
+        ArrayList<String> answer = new ArrayList<>();
+        
+        for(int i = 0; i < n; i++){
+            String str = picture[i];
+            String x = "";
+            
+            for(char ch : str.toCharArray()){
+                if (ch == 'x'){
+                    x += "x".repeat(k);
+                } else{
+                    x += ".".repeat(k);
+                }
+            }
+            
+            for(int j = 0; j < k; j++){
+                answer.add(x);
+            }
+
+        }
+        return answer.toArray(new String[answer.size()]);
+    }
+}
