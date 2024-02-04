0a20bc68c830519f227d46b7c8af0a0e0370f69c
SangJin Ham <rhkstlash@naver.com>
2024-02-04 23:21:19 +0900

    [level 0] Title: 세로 읽기, Time: 9.56 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -5,7 +5,7 @@ class Solution {
         int n = my_string.length();
         
         for(int i = 0; i < n; i += m){
-            answer += my_string.substring(i+c-1, i+c);
+            answer += my_string.charAt(i+c-1);
         }
         
         return answer;
