6859c9a8d20a0ab5dfc545be0bf4a78c0485c334
SangJin Ham <rhkstlash@naver.com>
2024-02-08 15:11:32 +0900

    [level 0] Title: 리스트 자르기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -3,17 +3,31 @@ import java.util.*;
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
         int a = slicer[0], b = slicer[1], c = slicer[2];
-        int n_4 = ((b-a) % 2 == 0) ? (b-a)/2 + 1 : (b-a+1)/2;
-        int[] answer_4 = new int[n_4];
+        
+        int[] answer = null;
         
         switch(n){
-            case 1: return Arrays.copyOfRange(num_list, 0, b+1);
-            case 2: return Arrays.copyOfRange(num_list, a, num_list.length);
-            case 3: return Arrays.copyOfRange(num_list, a, b+1);
-            case 4: for(int i = a, j = 0; i <= b; i+=2, j++){
-                answer_4[j] = num_list[i];
-            }
+            case 1: 
+                answer = Arrays.copyOfRange(num_list, 0, b+1);
+                break;
+                
+            case 2: 
+                answer = Arrays.copyOfRange(num_list, a, num_list.length);
+                break;
+                
+            case 3: 
+                answer = Arrays.copyOfRange(num_list, a, b+1);
+                break;
+                
+            case 4: 
+                answer = new int[((b-a) % 2 == 0) ? (b-a)/2 + 1 : (b-a+1)/2];
+                
+                for(int i = a, j = 0; i <= b; i+=2, j++){
+                    answer[j] = num_list[i];
+                }
+                break;
         }
-        return answer_4;
+        
+        return answer;
     }
 }
