a6bd4fba6f3deaa0ce601bb39ea3aeef06cd08ba
SangJin Ham <rhkstlash@naver.com>
2024-02-12 15:07:06 +0900

    [level 0] Title: 세 개의 구분자, Time: 45.39 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,27 +3,16 @@ import java.util.*;
 class Solution {
     public String[] solution(String myStr) {
         
-        ArrayList<String> strs = new ArrayList<>();
-        String temp = "";
+        ArrayList<String> list = new ArrayList<>();
+        String[] my_split = myStr.split("a|b|c");
         
-        for(char ch : myStr.toCharArray()){
-            // 구분자가 아닌 경우
-            if(ch != 'a' && ch != 'b' && ch != 'c'){
-                temp += String.valueOf(ch);             // 글자 추가해
-                
-            // 구분자이면서, temp가 비어있지 않은 경우
-            } else if(temp.length() != 0){
-                strs.add(temp);                         // 현재 temp 배열에 추가
-                temp = "";                              // temp 초기화
+        for(String str : my_split){
+            if(!str.isEmpty()){
+                list.add(str);
             }
         }
         
-        // 마지막에 구분자가 안 나왔을 경우 남아있는 temp 추가
-        if(temp.length() != 0){
-            strs.add(temp);
-        }
-        
-        String[] answer = strs.toArray(new String[0]);
+        String[] answer = list.toArray(new String[0]);
         
         return (answer.length != 0) ? answer : new String[]{"EMPTY"};
     }
