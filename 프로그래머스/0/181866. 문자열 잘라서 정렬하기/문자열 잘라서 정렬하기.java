4f95e4ec72cefc3820aeca2fdd30040a2e035a84
SangJin Ham <rhkstlash@naver.com>
2024-02-12 17:21:30 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 15.68 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -2,16 +2,15 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
-        
         // x를 기준으로 나누고 사전순으로 정렬
-        String[] str_split = myString.split("x");
-        String[] answer = {};
-        Arrays.sort(str_split);
+        String[] answer = myString.split("x");
+        
+        Arrays.sort(answer);
         
         // 빈 문자열이 아닌 문자열 개수 세서, 빈 문자열 제외하고 배열 복사
-        for(int i = 0; i < str_split.length; i++){
-            if(!str_split[i].equals("")){
-                answer = Arrays.copyOfRange(str_split, i, str_split.length);
+        for(int i = 0; i < answer.length; i++){
+            if(!answer[i].equals("")){
+                answer = Arrays.copyOfRange(answer, i, answer.length);
                 break;
             }
         }
