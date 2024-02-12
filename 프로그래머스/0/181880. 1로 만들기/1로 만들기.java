d84b55dafcd08f269046b964a299fc2a39e36aaa
SangJin Ham <rhkstlash@naver.com>
2024-02-12 13:59:25 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -5,7 +5,7 @@ class Solution {
         
         for(int i = 0; i < num_list.length; i++){
             while(num_list[i] != 1){
-                num_list[i] = (num_list[i] % 2 ==0) ? num_list[i] / 2 : (num_list[i] - 1) / 2;
+                num_list[i] /= 2;
                 answer ++;
             }
         }
