cf9b50c5ba0eec408aefe1c9ccd467db70fc0573
SangJin Ham <rhkstlash@naver.com>
2024-02-15 13:20:22 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int[n % 2 == 0 ? n/2 : n/2+1];
+        
+        for(int i = 1, j = 0; i <= n; i+= 2){
+            answer[j++] = i;
+        }
+        return answer;
+    }
+}
