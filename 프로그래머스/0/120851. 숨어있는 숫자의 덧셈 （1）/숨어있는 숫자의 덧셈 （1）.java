17f9700ad673ed1840dfcd3e0321779ff642e5e1
SangJin Ham <rhkstlash@naver.com>
2024-02-15 13:42:08 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(String my_string) {
         int answer = 0;        
-        for(int i = 0; i < my_string.length(); i++){
-            int n = (int) my_string.charAt(i);
-            if(n >= 48 && n <= 57){
-                answer += n - 48;
+        for(char ch : my_string.toCharArray()){
+            if(ch >= '0' && ch <= '9'){
+                answer += ch - '0';
             }
         }
         return answer;
