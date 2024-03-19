ad4c3fa3761fbc4575bccc6d32866aa5ccbefe29
SangJin Ham <rhkstlash@naver.com>
2024-03-19 15:00:00 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -1,25 +1,12 @@
 class Solution {
     public int solution(int n) {
-        
-        int answer = 0;
+        int answer = 0; 
         int fac = 1;
         
-        for(int i = 1; i <= 10; i++){
-            fac = 1;
-            
-            for(int j = 1; j <= i; j++){
-                fac *= j;
-            }
-            
-            if(fac == n){
-                answer = i;
-                break;
-            } else if(fac > n){
-                answer = i-1;
-                break;
-            }
+        while(n >= fac) {
+            fac *= ++answer;
         }
         
-        return answer;
+        return answer-1;
     }
 }
