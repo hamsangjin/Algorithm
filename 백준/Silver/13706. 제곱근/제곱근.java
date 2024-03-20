706197bbd59af00775b054ca724012c5eb57c8b2
SangJin Ham <rhkstlash@naver.com>
2024-03-20 15:32:11 +0900

    [Silver IV] Title: 제곱근, Time: 216 ms, Memory: 17820 KB -BaekjoonHub

---
+++ 백준/Silver/13706. 제곱근/제곱근.java
@@ -0,0 +1,11 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = new BigInteger(sc.next());
+
+        System.out.println(n.sqrt());
+    }
+}
