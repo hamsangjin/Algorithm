0d869357a744ee73a01bb61154d290eeab9acc49
SangJin Ham <rhkstlash@naver.com>
2024-03-25 22:24:37 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 7.62 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -3,26 +3,38 @@ import java.util.*;
 class Solution {
     public String solution(String[] survey, int[] choices) {
         
-        HashMap<String, Integer> scores = new HashMap<String, Integer>();
+        // 선택과 매칭되는 점수 정의
+        HashMap<Integer, Integer> scores = new HashMap<Integer, Integer>();
+        // for(int i = 1; i <= 7; i++){
+        //     if(i <= 3)      scores.put(i, 4-i);
+        //     if(i >= 5)      scores.put(i, i-4);
+        // }
+        scores.put(1, 3);scores.put(2, 2);scores.put(3, 1);scores.put(4, 0);
+        scores.put(5, 1);scores.put(6, 2);scores.put(7, 3);
         
-        scores.put("1", 3);scores.put("2", 2);scores.put("3", 1);scores.put("4", 0);
-        scores.put("5", 1);scores.put("6", 2);scores.put("7", 3);
-        
+        // 유형별 누적 점수 저장
         HashMap<String, Integer> categories = new HashMap<String, Integer>();
-        categories.put("R", 0);categories.put("T", 0);categories.put("C", 0);categories.put("F", 0);
-        categories.put("J", 0);categories.put("M", 0);categories.put("A", 0);categories.put("N", 0);
+        for(String category : new String[]{"R", "T", "C", "F", "J", "M", "A", "N"}){
+            categories.put(category, 0);
+        }
         
         for(int i = 0; i < survey.length; i++){
+            // 유형 두 개 나누기
             String c1 = "" + survey[i].charAt(0);
             String c2 = "" + survey[i].charAt(1);
             
+            // c1 유형에 더해지는 선택이냐?
             if(choices[i] <= 3){
-                categories.put(c1, categories.get(c1) + scores.get(Integer.toString(choices[i])));
+                // 그렇다면 c1의 점수를 꺼내서 해당 선택과 매칭되는 점수를 더해라
+                categories.put(c1, categories.get(c1) + scores.get(choices[i]));
+            // c2 유형에 더해지는 선택이냐 ?
             } else if(choices[i] >= 3){
-                categories.put(c2, categories.get(c2) + scores.get(Integer.toString(choices[i])));
+                // 그렇다면 c2의 점수를 꺼내서 해당 선택과 매칭되는 점수를 더해라
+                categories.put(c2, categories.get(c2) + scores.get(choices[i]));
             }
         }
         
+        // 유형별 더 큰 점수로 저장하되, 같은 경우 사전 순으로 빠른 유형으로 저장
         String answer = "";
         answer += (categories.get("R") < categories.get("T")) ? "T" : "R";
         answer += (categories.get("C") < categories.get("F")) ? "F" : "C";
