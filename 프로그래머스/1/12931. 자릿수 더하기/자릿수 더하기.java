31d6a19a6d86610ed3664a2696c6a09aa2af4781
SangJin Ham <rhkstlash@naver.com>
2024-03-25 20:12:43 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.10 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for(String str : Integer.toString(n).split("")) answer += Integer.parseInt(str);
+
+        return answer;
+    }
+}
