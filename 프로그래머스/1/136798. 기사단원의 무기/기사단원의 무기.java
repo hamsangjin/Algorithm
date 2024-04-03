2dba87ba0248fbcfcaf935585af4c4c0389be8e5
SangJin Ham <rhkstlash@naver.com>
2024-04-03 20:09:13 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.93 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,23 +1,24 @@
 class Solution {
     public int solution(int number, int limit, int power) {
         int answer = 0;
+        int[] arr= new int[number];
         
-        for(int i = 1; i <= number; i++){
-            int cnt = divisor(i);
-            answer += (cnt > limit) ? power : cnt;
+        for(int i=1; i <=number; i++){
+            int count=0;
+            for(int j=1; j*j<=i; j++){
+                if(j*j==i){
+                    count+=1;
+                } else if(i%j==0){
+                    count+=2;
+                }
+            }
+            arr[i-1]=count;
+            if(limit<arr[i-1]){
+                answer+=power;
+            }else{
+                answer+=arr[i-1];
+            }
         }
-            
         return answer;
-    }
-    
-    public static int divisor(int n){
-        int temp = 0;
-        
-        for (int i = 1; i*i <= n; i++){
-            if(i*i==n)      temp++;
-            else if(n%i==0) temp+=2;
-        }
-        
-        return temp;
     }
 }
