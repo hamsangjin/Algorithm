9504188d9dbe6232e78eff8b4535eb1d147a0b4d
SangJin Ham <rhkstlash@naver.com>
2024-04-04 22:04:17 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 68.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,20 +1,28 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        int sum = brown + yellow;
-        
-        for (int i = 3; i < sum; i++) {
-            int j = sum / i;
-            
-            if (sum % i == 0 && j >= 3) {
-                int x = Math.max(i, j);
-                int y = Math.min(i, j);
-                int center = (x - 2) * (y - 2);
-                
-                if (center == yellow) {
-                    answer[0] = x;
-                    answer[1] = y;
-                    return answer;
+ 
+        int carpet = brown + yellow;
+ 
+        // yellow가 존재하기 위해서는 가로와 세로의 길이가 3이상
+        for (int i = 3; i <= carpet; i++) {
+            int col = i;                // 세로
+            int row = carpet / col;     // 가로
+ 
+            // 가로의 길이가 3이상이 안 되는 경우
+            if (row < 3) {
+                continue;
+            }
+ 
+            // 카펫의 가로 길이는 세로 길이와 같거나, 세로 길이보다 깁니다.
+            if (row >= col) {
+                // yellow의 길이
+                // - 가로: 카펫의 가로 길이 - 2
+                // - 세로: 카펫의 세로 길이 - 2
+                if ((row - 2) * (col - 2) == yellow) {
+                    answer[0] = row;
+                    answer[1] = col;
+                    break;
                 }
             }
         }
