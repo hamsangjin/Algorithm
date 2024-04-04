834d70b1b5214e86ecb3266dd517a41cf4ebc136
SangJin Ham <rhkstlash@naver.com>
2024-04-05 00:24:39 +0900

    [level 2] Title: 할인 행사, Time: 0.03 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -3,25 +3,29 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        
-        // 원하는 품목의 개수를 hashmap을 이용해 저장함
-        // ex) "banana: 3"
-        HashMap<String, Integer> want_map = new HashMap<>();
-        for(int i = 0; i < want.length; i++){
-            want_map.put(want[i], number[i]);
+
+        HashMap<String, Integer> wantMap = new HashMap<>();
+        for (int i = 0; i < want.length; i++) {
+            wantMap.put(want[i], number[i]);
         }
+
+        HashMap<String, Integer> tempMap = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            tempMap.put(discount[i], tempMap.getOrDefault(discount[i], 0) + 1);
+        }
+
+        int cur1 = 0;
+        int cur2 = 9;
+        if (wantMap.equals(tempMap)) answer++;
         
-        // number의 원소의 합은 10이므로,
-        // discount 배열의 원소를 0~9, 1~10, 2~11 이런 식으로 확인
-        for(int i = 0; i < discount.length-9; i++){
-            // i ~ i+9까지의 품목의 개수 구하기
-            HashMap<String, Integer> tempMap = new HashMap<>();
-            for(int j = i; j < i+10; j++){
-                tempMap.put(discount[j], tempMap.getOrDefault(discount[j], 0) + 1);
-            }
-            
-            // 원하는 품목의 개수와 같은 경우
-            if(want_map.equals(tempMap)) answer ++;
+        while (cur2 < discount.length-1) {
+            tempMap.put(discount[cur1], tempMap.getOrDefault(discount[cur1], 0) - 1);
+            if (tempMap.get(discount[cur1]) == 0) tempMap.remove(discount[cur1]);
+            cur1++;
+            cur2++;
+            tempMap.put(discount[cur2], tempMap.getOrDefault(discount[cur2], 0) + 1);
+
+            if (wantMap.equals(tempMap)) answer++;
         }
         return answer;
     }
