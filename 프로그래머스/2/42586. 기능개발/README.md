7dd50c12cb5baa4ee4b2f3bae825ceb119a35b83
SangJin Ham <rhkstlash@naver.com>
2024-04-11 21:54:15 +0900

    [level 2] Title: 기능개발, Time: 2.27 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 2.17 ms
+메모리: 81 MB, 시간: 2.27 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 21일 09:50:43
+2024년 04월 11일 21:54:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -6,21 +6,30 @@ class Solution {
         ArrayList<Integer> answer = new ArrayList<>();
 
         int day = 1;
-        int index = 0;
         int temp = 0;
-        while(index < progresses.length){
-            if(progresses[index] + speeds[index]*day >= 100){
+        // 하나씩 기능 해결
+        for(int i = 0; i < progresses.length; i++){
+            // 기능이 현재 day로 해결가능하냐 ?
+            if(progresses[i] + speeds[i]*day >= 100){
+                // 그렇다면 현재날짜까지 기능이 해결가능한 개수 +1
                 temp ++;
-                index ++;
-                continue;
+            // 현재 기능이 해결 불가능이냐? 
+            } else{
+                // 현재 날짜까지 해결된 기능이 하나라도 있냐 ?
+                if(temp > 0){
+                    // 그렇다면 return할 리스트에 현재 날짜까지 해결한 기능의 개수인 temp를 추가해주고 0으로 초기화.
+                    answer.add(temp);
+                    temp = 0;
+                }
+                
+                // 다시 해당 기능을 해결하기 위해 날짜(day)는 늘리고, 
+                // i-- 연산을 통해 같은 기능을 또 비교
+                i--;
+                day++;
             }
-            if(temp != 0){
-                answer.add(temp);
-                temp = 0;
-            }
-            day ++;
         }
-
+        
+        // 마지막으로 해결된 기능 else문 실행이 안되므로 여기서 추가
         if(temp != 0)   answer.add(temp);
 
         return answer.stream().mapToInt(i -> i).toArray();
