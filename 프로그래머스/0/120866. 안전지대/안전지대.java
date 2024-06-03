9c4e0d43fe634d5572ec8469fdf20a75a3bdde8d
SangJin Ham <rhkstlash@naver.com>
2024-06-03 21:31:40 +0900

    [level 0] Title: 안전지대, Time: 0.05 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,20 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[][] board) {
         int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
         int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
         
         int n = board.length;
-        int answer = 0;
+        int answer = n*n;
+        int cnt = 0;
         
         for(int r = 0; r < n; r++){
             for(int c = 0; c < n; c++){
                 if(board[r][c] == 1){
+                    cnt ++;
                     for(int i = 0; i < 8; i++){
                         int nr = r + dr[i];
                         int nc = c + dc[i];
                         if(nr >= 0 && nr < n && nc >= 0 && nc < n && board[nr][nc] == 0){
+                            cnt ++;
                             board[nr][nc] = 2;
                         }
                     }
@@ -22,13 +24,6 @@ class Solution {
                 
             }
         }
-        
-        for(int r = 0; r < n; r++){
-            for(int c = 0; c < n; c++){
-                if(board[r][c] == 0)    answer++;
-            }
-        }
-        
-        return answer;
+        return answer-cnt;
     }
 }
