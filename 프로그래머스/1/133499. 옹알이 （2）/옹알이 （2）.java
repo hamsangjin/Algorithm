54bea2b0018fbe2c91505c21d3cc3d8ad66e952d
SangJin Ham <rhkstlash@naver.com>
2024-06-21 16:04:47 +0900

    [level 1] Title: 옹알이 (2), Time: 2.35 ms, Memory: 68.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -1,12 +1,10 @@
 class Solution {
     public int solution(String[] babbling) {
-        String[] words = {"aya", "ye", "woo", "ma"};
         int answer = 0;
         
         for(String s : babbling){
             if(!s.contains("ayaaya") && !s.contains("yeye") && !s.contains("woowoo") && !s.contains("mama")){
-                for(int i = 0; i < 4; i++)  s = s.replace(words[i], " ");
-                s = s.replace(" ", "");
+                s = s.replaceAll("aya|ye|woo|ma", " ").replace(" ", "");
             }
             if(s.length() == 0)    answer++;
         }
