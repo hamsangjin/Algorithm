78e4cba3fdb5957a1794958121dd54161ba4b7c0
SangJin Ham <rhkstlash@naver.com>
2024-09-14 15:45:23 +0900

    [Silver IV] Title: 괄호, Time: 124 ms, Memory: 15356 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,24 +1,33 @@
-import java.util.Scanner;
+import java.io.*;
 import java.util.Stack;
 
+// https://code.plus/course/41
+// https://www.acmicpc.net/problem/9012
+// 200 - 자료구조 1 -> 괄호
+// 시간 복잡도: O(n * L)
+// - L: 입력된 괄호들의 길이
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for (int i = 0; i < n; i++) {
-            String ps = sc.next();
-            Stack<String> st = new Stack<>();
-            for(String x : ps.split("")){
-                if(!st.isEmpty() && st.peek().equals("(") && x.equals(")")){
-                    st.pop();
-                } else{
-                    st.push(x);
-                }
+        // n 입력
+        int n = Integer.parseInt(br.readLine());
+
+        // n번 반복
+        for(int i = 0; i < n; i++) {
+            Stack<String> s = new Stack<>();
+
+            // 괄호 입력
+            String[] in = br.readLine().split("");
+            for(String str : in) {
+                // 입력이 )인 경우, 스택이 비어있지 않고 가장 최상단의 괄호가 (이면 괄호가 쌍이 이루어진 것이므로 괄호 쌍 제거
+                if(str.equals(")") && !s.isEmpty() && s.peek().equals("("))     s.pop();
+                // 아니면 스택에 추가
+                else                                                        s.push(str);
             }
 
-            if(st.isEmpty())    System.out.println("YES");
-            else    System.out.println("NO");
+            if(s.isEmpty())     System.out.println("YES");
+            else                System.out.println("NO");
         }
     }
 }
