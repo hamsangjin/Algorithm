d1a2a3fb9f60a9dceb1bfcbc0b12af3d30b55e3e
SangJin Ham <rhkstlash@naver.com>
2024-11-06 16:16:31 +0900

    [D3] Title: 진기의 최고급 붕어빵, Time: 240 ms, Memory: 33,292 KB -BaekjoonHub

--- SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
+++ SWEA/D3/1860. 진기의 최고급 붕어빵/진기의 최고급 붕어빵.java
@@ -1,39 +1,37 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
-	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		try {
-			int T = Integer.parseInt(br.readLine());
-			for(int t = 1; t <= T; t++) {
-				String[] nmk = br.readLine().split(" ");
-				int n = Integer.parseInt(nmk[0]);
-				int m = Integer.parseInt(nmk[1]);
-				int k = Integer.parseInt(nmk[2]);
-				
-				String[] temp = br.readLine().split(" ");
-				int[] arr = new int[n];
-				for(int i = 0; i < n; i++) {
-					arr[i] = Integer.parseInt(temp[i]);
-				}
-				
-				Arrays.sort(arr);
-				
-				int f = 0;
-				String result = "Possible";
-				for(int i = 0; i < n; i++) {
-					f = (arr[i] / m) * k; 		// 손님이 온 시간이 됐을 때 붕어빵 몇 개 만들어졌는지
-					
-					if(f-i-1 < 0 ) { 			// i는 앞에 손님들의 수 
-						result = "Impossible";
-						break;
-					}
-				}
-				
-				System.out.printf("#%d %s\n", t, result);
-			}
-		} catch (IOException e) {}
-	}
+	public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= T; t++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            int[] arr = new int[N];
+            st = new StringTokenizer(br.readLine());
+            for(int i = 0; i < N; i++)		arr[i] = Integer.parseInt(st.nextToken());
+            Arrays.sort(arr);
+            
+            int b = 0;
+            String ans = "Possible";
+			for(int i = 0; i < N; i++){
+                // arr[i]초가 지나면 만들 수 있는 붕어빵 개수
+                b = (arr[i] / M) * K;
+                
+                // arr[i]초가 됐을 때 지금까지 왔던 인원 수(i) + 현재 손님(1)만큼 붕어빵 개수를 뺐을 때,
+                // 붕어빵 개수가 음수가 된다면 붕어빵을 제공할 수 없다는 뜻
+                // ans에 Impossible을 저장하고 for문 탈출
+                if(b-i-1 < 0){
+                    ans = "Impossible";
+                    break;
+                }
+            }
+            System.out.printf("#%d %s\n", t, ans);
+        }
+    }
 }
