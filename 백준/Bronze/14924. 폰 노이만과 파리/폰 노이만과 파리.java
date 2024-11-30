181391a1cded62f2c47c8b3c3edc3699582c6b14
SangJin Ham <rhkstlash@naver.com>
2024-11-30 11:43:35 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 164 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int s = sc.nextInt(), t = sc.nextInt(), d = sc.nextInt();
+        System.out.println(d/(2*s)*t);
+    }
+}
