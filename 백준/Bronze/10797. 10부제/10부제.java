d8d2226ea1b65c439843285c29cb846498fe8af9
SangJin Ham <rhkstlash@naver.com>
2024-12-04 22:14:58 +0900

    [Bronze IV] Title: 10부제, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int count = 0;
+		
+		for(int i = 0; i < 5; i++) {
+			int car = Integer.parseInt(st.nextToken());
+			if(car == N) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+
+}
