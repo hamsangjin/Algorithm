b771ae0db863827f14ce35f371fa1c0244b25769
SangJin Ham <rhkstlash@naver.com>
2024-12-26 20:42:18 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.09 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -5,28 +5,20 @@ class Solution {
         int num = 0, L = 0;
 
         Map<String, Boolean> map = new HashMap<>();
+        int[] answer = new int[]{0, 0};
         
-        boolean flag = true;
         for(int i = 0; i < words.length; i++){
             // 첫 번째인 경우 아무 제약이 없으므로 단어만 추가하고 넘어가기
             // 이미 단어를 말했거나 이전 단어의 마지막 문자로 시작하지 않는 경우
             if(i >= 1 &&  (map.containsKey(words[i]) || words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0))){
-                num = i % n + 1;
-                L = i / n + 1;
-                flag = false;
+                answer[0] = i % n + 1;
+                answer[1] = i / n + 1;
                 break;
             }else{
                 map.put(words[i], true);
                 continue;
             }
         }
-        
-        int[] answer;
-        
-        // 다 돌은 경우
-        if(flag)    answer = new int[]{0, 0};
-        else        answer = new int[]{num, L};
-        
         return answer;
     }
 }
